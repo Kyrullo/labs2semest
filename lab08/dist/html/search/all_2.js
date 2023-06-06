9c77b624ee374f388ed2bdb2f9b73884424e1853
kyryllo <115114161+Kyrullo@users.noreply.github.com>
2023-06-06 21:39:56 +0300

    Delete lab08 directory

--- lab08/dist/html/search/all_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['squarematrix_0',['squareMatrix',['../main_8c.html#a814246cc9d17dbda7c2b8e9c26b64b94',1,'main.c']]]
-];
--- lab08/dist/html/search/functions_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['primenumber_0',['primenumber',['../main_8c.html#ab939ed5be71cf34184ef8fecd4382f6a',1,'main.c']]]
-];
